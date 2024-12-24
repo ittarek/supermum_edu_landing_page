b4033aecfb928194ceafe1d7a70dedba7edfd94f
MD Tariqul Islam <ittarek551@gmail.com>
2024-12-24 18:27:51 +0600

    add platform details

--- src/Components/Platform/Platform.jsx
+++ src/Components/Platform/Platform.jsx
@@ -1,10 +1,16 @@
 import "./Platform.css"
+// images
 import platformImg from "../../assets/images/platform_image/userMain.png"
 import people1 from "../../assets/images/platform_image/user2.png"
 import people2 from "../../assets/images/platform_image/user3.png"
 import people3 from "../../assets/images/platform_image/user4.png"
 import people4 from "../../assets/images/platform_image/user5.png"
 import circle from "../../assets/images/platform_image/circle.png"
+// icons
+import { FaVideo } from "react-icons/fa";
+import { LuCalendarRange } from "react-icons/lu";
+import { IoMdBook } from "react-icons/io";
+import { TbSocial } from "react-icons/tb";
 const peopleImage = [
      {id:1, img: people1},
      {id:2, img: people2},
@@ -12,9 +18,40 @@ const peopleImage = [
      {id:4, img: people4},
      
 ]
+// card data
+const platformCardData = [
+  {
+    id: 1,
+    title: "Virtual learning",
+    details:
+      "The only costs are for standard school supplies and voluntary field trips.",
+    icon: <><FaVideo/></>,
+  },
+  {
+    id: 2,
+    title: "Meaningful Education",
+    details:
+      "Many of our Connections Academy schools have additional accreditations.",
+    icon: <><LuCalendarRange/></>,
+  },
+  {
+    id: 3,
+    title: "ONLINE CURRICULUM",
+    details:
+      "Our curriculum prepares students to go further in life by giving them support.",
+    icon: <><IoMdBook/></>,
+  },
+  {
+    id: 4,
+    title: "SOCIAL INTERACTION",
+    details:
+      "Students at Connections Academy collaborate on projects together.",
+    icon: <><TbSocial/></>,
+  },
+];
 const Platform = () => {
      return (
-       <div id="platform_container ">
+       <div id="platform_container">
          {/*left side component  */}
          <div className="left_component">
            {/* image div */}
@@ -29,17 +66,39 @@ const Platform = () => {
                {/* people image */}
                <div className="people_img">
                  {peopleImage.map((data, index) => (
-                   <div key={data.id} className="image_stack" style={{ left: `${index * -15}px` }}>
+                   <div
+                     key={data.id}
+                     className="image_stack"
+                     style={{ left: `${index * -15}px` }}
+                   >
                      <img src={data.img} alt="People are learning" />
                    </div>
                  ))}{" "}
                  <button className="show_more_button">3+</button>
                  {/* circle */}
                  <div className="circle_image">
-                    <img src={circle} alt="circle image" />
+                   <img src={circle} alt="circle image" />
                  </div>
                </div>
              </div>
+           </div>
+         </div>
+
+         {/* right side component */}
+         <div className="right_side_component">
+           <h1>How online public school works </h1>
+           <div className="platform_card">
+             {platformCardData.slice( 0, 4).map(data => (
+               <div key={data.id} className="platform_data">
+                    <span>
+                         {data.icon}
+                    </span>
+      <h4>{data.title}</h4>
+      <p>{data.details}</p>
+
+
+               </div>
+             ))}
            </div>
          </div>
        </div>
