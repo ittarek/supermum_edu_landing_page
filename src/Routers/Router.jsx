7602ce3ac59ab4e93ea8699333507923af3151f5
MD Tariqul Islam <ittarek551@gmail.com>
2024-12-26 22:48:00 +0600

    add build

--- src/Routers/Router.jsx
+++ src/Routers/Router.jsx
@@ -3,19 +3,16 @@ import ErrorPage from "../Components/ErrorElemnt/ErrorPage";
 import Home from "../page/home/Home";
 import Layout from "../layout/Layout";
 
-
-
 export const router = createBrowserRouter([
-     {
-          errorElement:<ErrorPage/>,
-          path: "/",
-          element: <Layout/>,
-          children:[
-
-               {
-path: "/",
-element: <Home/>
-               }
-          ]
-     }
-])
+  {
+    errorElement: <ErrorPage />,
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
